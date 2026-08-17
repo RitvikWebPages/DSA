827b9403748674446300c72e8a3e6f6e765b7323
RitvikWebPages <76900652+RitvikWebPages@users.noreply.github.com>
2026-08-17 15:46:50 +0530

    Time: 3 ms (28.60%) | Memory: 10 MB (35.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,8 +5,8 @@ public:
         int left = 0;
 
         while(right > left){
-            while (left < right && !isalnum(s[left])) left++;
-            while (left < right && !isalnum(s[right])) right--;
+            while (left < right && (s[left] < '0' || (s[left] > '9' && s[left] < 'A') || (s[left] > 'Z' && s[left] < 'a') || s[left] > 'z')) left++;
+            while (left < right && (s[right] < '0' || (s[right] > '9' && s[right] < 'A') || (s[right] > 'Z' && s[right] < 'a') || s[right] > 'z')) right--;
             if(tolower(s[right]) != tolower(s[left])){
                 return false;
             }
