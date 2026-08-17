d97e3fde0a23c886fbeca0b4c288784c3aa5aee7
RitvikWebPages <76900652+RitvikWebPages@users.noreply.github.com>
2026-08-17 14:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,19 +3,17 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> answer(n, 1);
-        
-        int running = 1;
-        for (int i = 0; i < n; i++) {
-            answer[i] = running;
-            running *= nums[i];
+        int prefix = 1;
+        for(int i =0;i <n; i++){
+            answer[i] = prefix;
+            prefix *= nums[i];
         }
-        
-        running = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= running;
-            running *= nums[i];
+        int postfix = 1;
+        for(int i =n - 1;i >= 0; i--){
+            answer[i] *= postfix;
+            postfix *= nums[i];
         }
-        
         return answer;
+
     }
 };
